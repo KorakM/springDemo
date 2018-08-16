9e9424c9ca63e568da3f0d384e373e86a444f04a
Korak Mitra <korak.mitra.pro@gmail.com>
2018-08-16 23:54:42 +0530

    Spring demo phase 1

--- src/com/love2code/springDemo/MyApp.java
+++ src/com/love2code/springDemo/MyApp.java
@@ -2,7 +2,7 @@ package com.love2code.springDemo;
 
 public class MyApp {
     public static void main(String[] args) {
-        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
 
         System.out.println("daily workout = " + theCoach.getDailyWorkout());
     }
