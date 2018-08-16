76e70a75fdbdf5abae527ebcf15c58ffb10de6c9
Korak Mitra <korak.mitra.pro@gmail.com>
2018-08-17 01:09:38 +0530

    Spring demo - Running he first spring app

--- src/com/love2code/springDemo/BaseballCoach.java
+++ src/com/love2code/springDemo/BaseballCoach.java
@@ -4,6 +4,6 @@ public class BaseballCoach implements Coach {
 
     @Override
     public String getDailyWorkout(){
-        return "Spend 30 minutes in batting practise";
+        return "Spend 30 minutes in batting practice";
     }
 }
---
+++ src/com/love2code/springDemo/HelloSpringApp.java
@@ -0,0 +1,21 @@
+package com.love2code.springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args) {
+
+        //load spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //retrieve bean fro spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        //call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        //close the context
+        context.close();
+    }
+}
